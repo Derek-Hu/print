043eb855f99fb9a0641fdd282460563454ba0d46
慧华 <hubenlv.hbl@antgroup.com>
2022-01-30 17:50:36 +0800

    finish

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { TextArea, Tag, Input, Toast, Slider, Form, Button } from 'antd-mobile';
+import { TextArea, Tag, Input, ActionSheet, Toast, Slider, Form, Button } from 'antd-mobile';
 import { useEffect, useState } from 'react';
 import styles from './index.less';
 
@@ -27,19 +27,31 @@ const tagColors = ['primary', 'success', 'warning', 'danger', '#2db7f5', '#87d06
 const CacheKey = 'PRINT_SETTINGS';
 const SizeKey = 'PRINT_SIZE_SETTINGS';
 const AdjustLevelKey = 'PRINT_ADJUST_SETTINGS';
+const FontKey = 'PRINT_FONT_SETTINGS';
 
 const firstW = 30;
 const firstH = 40;
 
 const defaultTemplate = ['25*25', '30*30', '30*40', '40*40', '50*40', '110*50', '120*60', '130*70'];
 const MaxTag = 8;
+
+const basicColumns = [
+  { text: '隶书', key: 'LiSu' },
+  { text: '魏碑体', key: `'Weibei SC','FZWeiBei-S03S'` },
+  { text: '楷书', key: `KaiTi, STKaiti, 'FZKai-Z03S', ` },
+  { text: '行书', key: `STXingkai, 'Xingkai SC'` },
+];
+
 export default function IndexPage() {
   const [form] = Form.useForm();
 
+  const [fontSheetVisible, setFontSheetVisible] = useState(false);
+  const [fontFamily, setFontFamily] = useState(basicColumns[0]);
   const [settings, setSettings] = useState({});
   const [selectTag, setSelectTag] = useState('');
+  const [textChanged, setTextChanged] = useState(true);
   const [adjustLevel, setAdjustLevel] = useState(defaultMark);
-  const [sizeSettings, setSizeSettings] = useState(defaultTemplate.slice(0, MaxTag));
+  const [sizeSettings, setSizeSettings] = useState<string[]>([]);
 
   const onSliderChange = (value: any) => {
     console.log('onSliderChange, ', value);
@@ -48,9 +60,29 @@ export default function IndexPage() {
     localStorage.setItem(AdjustLevelKey, level);
   };
 
-  const calculate = (values: { width: number; height: number; text: string }) => {
-    const { width: ww, height: wh, text } = values;
+  const onValuesChange = (values: any) => {
+    if ('text' in values) {
+      setTextChanged(true);
+    }
+  };
 
+  const onAction = (action: any) => {
+    console.log();
+    setFontFamily(action);
+    setFontSheetVisible(false);
+    localStorage.setItem(FontKey, JSON.stringify(action));
+  };
+  const calculate = (values: { width: number; height: number; text: string }) => {
+    const { width: ww, height: wh, text = '' } = values;
+    if (text == null || text === undefined) {
+      return;
+    }
+    const content = String.prototype.trim.call(text);
+
+    if (content === '') {
+      return;
+    }
+    setTextChanged(false);
     if (ww > A4.w || wh > A4.h) {
       Toast.show({
         icon: 'fail',
@@ -74,7 +106,6 @@ export default function IndexPage() {
     // height/ 297 = y / a4H
     const renderH = Math.floor((a4H * height) / A4.h);
 
-    const content = String.prototype.trim.call(text);
     const pageSize = Math.ceil(content.length / (columns * rows));
     const actualCols = Math.ceil(content.length / rows);
 
@@ -112,6 +143,7 @@ export default function IndexPage() {
     setSettings(configuration);
 
     console.log('settings:', configuration);
+    console.log('sizeSettings:', sizeSettings);
 
     localStorage.setItem(CacheKey, JSON.stringify(configuration));
 
@@ -131,7 +163,15 @@ export default function IndexPage() {
   };
 
   const onFinish = (values: { width: number; height: number; text: string }) => {
+    console.log('onFinish', values);
     calculate(values);
+    setTimeout(() => {
+      const settingsArea = document.getElementById('settingsArea');
+      if (settingsArea) {
+        const h = settingsArea.getBoundingClientRect().height;
+        window.scrollTo(0, h);
+      }
+    }, 0);
   };
 
   const clickTag = (template: string) => {
@@ -152,14 +192,41 @@ export default function IndexPage() {
     try {
       const configuration = JSON.parse(localStorage.getItem(CacheKey) as string);
       const sizeTemplate = JSON.parse(localStorage.getItem(SizeKey) as string);
+      const font = JSON.parse(localStorage.getItem(FontKey) as string);
       const adjustLevel = parseInt(localStorage.getItem(AdjustLevelKey) as string);
       console.log('cache: ', configuration);
       setSettings(configuration);
+      setFontFamily(font || basicColumns[0]);
       setAdjustLevel(isNaN(adjustLevel) ? defaultMark : adjustLevel);
-      if (Array.isArray(sizeTemplate)) {
-        setSizeSettings(sizeTemplate.filter(v => /\d+\*\d+/.test(v)).slice(0, 8));
+      let templates = [];
+      if (Array.isArray(sizeTemplate) && sizeTemplate.length) {
+        templates = sizeTemplate.filter(v => /\d+\*\d+/.test(v)).slice(0, 8);
       }
-      calculate(configuration);
+
+      if (!templates.length) {
+        templates = defaultTemplate.slice(0, MaxTag);
+      }
+      console.log('configuration: ', configuration);
+      console.log('tag222: ', templates);
+      setSizeSettings(templates as any);
+      const { width = 0, text = '', height = 0 } = configuration || {};
+      form.setFieldsValue({
+        text,
+      });
+
+      const tag = `${width}*${height}`;
+      console.log('tag: ', tag);
+
+      if (templates.indexOf(tag) !== -1) {
+        setTimeout(() => {
+          clickTag(tag);
+        });
+      } else {
+        setTimeout(() => {
+          clickTag(`${firstW}*${firstH}`);
+        });
+      }
+      // calculate(configuration);
     } catch (e) {}
   }, []);
 
@@ -189,136 +256,167 @@ export default function IndexPage() {
    * 2: 1 --> 210: 297 / 2
    */
 
-  const [iw, ih] =
-    sizeSettings && sizeSettings[0] ? sizeSettings[0].split('*').map(v => parseFloat(v)) : [firstW, firstH];
+  const [iw, ih] = selectTag ? selectTag.split('*').map(v => parseFloat(v)) : [firstW, firstH];
+  console.log('sizeSettings', sizeSettings);
+  console.log('selectTag', selectTag);
+
+  console.log('iw, ih: ', iw, ih);
+
   return (
     <div className={styles.root}>
       <div className={styles.settings}>
-        <h1 className={styles.title}>文字打印 隶书</h1>
-        <Form
-          form={form}
-          initialValues={{
-            width: iw,
-            height: ih,
-            text: '故人西辞黄鹤楼',
-            // text: '故'
-          }}
-          // onValuesChange={onValuesChange}
-          onFinish={onFinish}
-          footer={
-            <Button block type="submit" color="primary" size="large">
-              预&nbsp;&nbsp;览
-            </Button>
-          }
-          autoComplete="off"
-        >
-          <Form.Item
-            name="width"
-            label="单字宽度/宽度 5 ~ 210（毫米）"
-            rules={[{ required: true, message: '请输入单个汉字宽度' }]}
-          >
-            <Input type="number" min={5} max={A4.w} placeholder="最小宽度5mm，最大宽度210mm" />
+        <div id="settingsArea">
+          <h1 className={styles.title} style={{fontFamily: fontFamily && fontFamily.key ? fontFamily.key : 'LiSu'}}>文字打印</h1>
+          <Form.Item label="字体" onClick={() => setFontSheetVisible(true)}>
+            <span className={styles.fontName}>{fontFamily && fontFamily.text}</span>
           </Form.Item>
 
-          <Form.Item
-            name="height"
-            label="单字高度/高度 5 ~ 297（毫米）"
-            rules={[{ required: true, message: '请输入单个汉字高度' }]}
-          >
-            <Input type="number" min={5} max={A4.h} placeholder="最小高度5mm，最大高度297mm" />
-          </Form.Item>
-          <Form.Item label="打印的文字" name="text" rules={[{ required: true, message: '请输入要打印的文字' }]}>
-            <TextArea
-              showCount={length => <span className={styles.count}>字数统计：共 {length} 个字</span>}
-              rows={3}
-              placeholder="点击输入要打印的内容"
-            />
-          </Form.Item>
-        </Form>
+          <ActionSheet
+            extra="请选择打印的字体"
+            onAction={onAction}
+            cancelText='取消'
+            visible={fontSheetVisible}
+            actions={basicColumns}
+            onClose={() => setFontSheetVisible(false)}
+          />
+          {sizeSettings && sizeSettings.length ? (
+            <>
+              <Form.Item label="常用尺寸">
+                <div className={styles.tagList}>
+                  {sizeSettings.slice(0, MaxTag).map((template, idx) => {
+                    return (
+                      <Tag
+                        className={selectTag === template ? styles.tagActive : ''}
+                        key={idx}
+                        onClick={() => clickTag(template)}
+                        color={selectTag === template ? 'danger' : 'default'}
+                      >
+                        {template}
+                      </Tag>
+                    );
+                  })}
+                </div>
+              </Form.Item>
+            </>
+          ) : null}
 
-        {pageSize ? (
-          <Form.Item label="文字间距微调">
-            <Slider value={adjustLevel} onChange={onSliderChange} ticks={true} marks={marks} min={0} max={6} />
-          </Form.Item>
-        ) : null}
-
-        {pageSize && sizeSettings && sizeSettings.length ? (
-          <>
-            <Form.Item label="快速调节文字宽高">
-              <div className={styles.tagList}>
-                {sizeSettings.slice(0, MaxTag).map((template, idx) => {
-                  return (
-                    <Tag
-                      className={selectTag === template ? styles.tagActive : ''}
-                      key={idx}
-                      onClick={() => clickTag(template)}
-                      color={tagColors[idx]}
-                    >
-                      {template}
-                    </Tag>
-                  );
-                })}
-              </div>
-            </Form.Item>
-            <Form.Item>
-              <Button
-                onClick={() => {
-                  window.print();
-                }}
-                block
-                color="primary"
-                size="large"
-              >
-                打&nbsp;&nbsp;印
+          <Form
+            form={form}
+            initialValues={{
+              width: iw,
+              height: ih,
+              text: '',
+              // text: '故'
+            }}
+            onValuesChange={onValuesChange}
+            onFinish={onFinish}
+            footer={
+              <Button block type="submit" color="primary" size="large">
+                预览（{fontFamily && fontFamily.text ? fontFamily.text : '隶书'}字体）
               </Button>
+            }
+            autoComplete="off"
+          >
+            {/* <Form.Item name="font" label="字体">
+              <Picker columns={basicColumns} />
+            </Form.Item> */}
+
+            <Form.Item
+              name="width"
+              label="单字宽度/宽度 5 ~ 210（毫米）"
+              rules={[{ required: true, message: '请输入单个汉字宽度' }]}
+            >
+              <Input type="number" min={5} max={A4.w} placeholder="最小宽度5mm，最大宽度210mm" />
             </Form.Item>
+
+            <Form.Item
+              name="height"
+              label="单字高度/高度 5 ~ 297（毫米）"
+              rules={[{ required: true, message: '请输入单个汉字高度' }]}
+            >
+              <Input type="number" min={5} max={A4.h} placeholder="最小高度5mm，最大高度297mm" />
+            </Form.Item>
+            <Form.Item label="打印的文字" name="text" rules={[{ required: true, message: '请输入要打印的文字' }]}>
+              <TextArea
+                showCount={length => <span className={styles.count}>字数统计：共 {length} 个字</span>}
+                rows={3}
+                placeholder="点击输入要打印的内容"
+              />
+            </Form.Item>
+          </Form>
+        </div>
+
+        {textChanged ? null : (
+          <>
+            {pageSize ? (
+              <Form.Item>
+                <Button
+                  onClick={() => {
+                    window.print();
+                  }}
+                  block
+                  color="primary"
+                  size="large"
+                >
+                  打印全部（共{pageSize}页）
+                </Button>
+              </Form.Item>
+            ) : null}
+
+            {pageSize ? (
+              <Form.Item label="文字间距微调">
+                <Slider value={adjustLevel} onChange={onSliderChange} ticks={true} marks={marks} min={0} max={6} />
+              </Form.Item>
+            ) : null}
           </>
-        ) : null}
+        )}
       </div>
 
-      <div className={styles.kedu} style={{ paddingLeft: `${paddingLeft}px` }}>
-        <div className={styles.privewAreaA4}>
-          {Array.from({ length: pageSize }).map((_p, pageIdx) => {
-            return (
-              <div key={pageIdx} className={styles.a4} style={{ height: `${a4H}px` }}>
-                <div style={{ paddingTop: `${gap}px` }}>
-                  {Array.from({ length: rows }).map((_r, rowIdx) => (
-                    <div key={rowIdx} className={styles.row}>
-                      {Array.from({ length: columns }).map((_c, colIdx) => {
-                        return (
-                          <div
-                            key={colIdx}
-                            className={[styles.col, actualCols > colIdx ? '' : styles.emptyWord].join(' ')}
-                            style={{
-                              // fontSize: `${Math.min(renderW, renderH)}px`,
-                              width: `${renderW}px`,
-                              height: `${renderH}px`,
-                            }}
-                          >
-                            <span
+      {textChanged ? null : (
+        <div className={styles.kedu} style={{ paddingLeft: `${paddingLeft}px` }}>
+          <div className={styles.privewAreaA4} style={{fontFamily: fontFamily && fontFamily.key ? fontFamily.key : 'LiSu'}}>
+            {Array.from({ length: pageSize }).map((_p, pageIdx) => {
+              return (
+                <div key={pageIdx} className={styles.a4} style={{ height: `${a4H}px` }}>
+                  <div style={{ paddingTop: `${gap}px` }}>
+                    {Array.from({ length: rows }).map((_r, rowIdx) => (
+                      <div key={rowIdx} className={styles.row}>
+                        {Array.from({ length: columns }).map((_c, colIdx) => {
+                          return (
+                            <div
+                              key={colIdx}
+                              className={[styles.col, actualCols > colIdx ? '' : styles.emptyWord].join(' ')}
                               style={{
-                                fontSize: `${baseSize}px`,
-                                transform: `scale(${renderW / baseSize}, ${
-                                  renderH / baseSize + (adjustLevel / 10) * (renderW / baseSize) * (height / width)
-                                })`,
+                                // fontSize: `${Math.min(renderW, renderH)}px`,
+                                width: `${renderW}px`,
+                                height: `${renderH}px`,
                               }}
                             >
-                              {text[getTextIndex(pageIdx, rowIdx, colIdx)] || ''}
-                            </span>
-                          </div>
-                        );
-                      })}
-                      <span className={styles.rowIndex} style={{ height: `${renderH}px` }}>
-                        {rowIdx + 1}
-                      </span>
-                    </div>
-                  ))}
+                              <span
+                                style={{
+                                  fontSize: `${baseSize}px`,
+                                  transform: `scale(${renderW / baseSize}, ${
+                                    renderH / baseSize + (adjustLevel / 10) * (renderW / baseSize) * (height / width)
+                                  })`,
+                                }}
+                              >
+                                {text[getTextIndex(pageIdx, rowIdx, colIdx)] || ''}
+                              </span>
+                            </div>
+                          );
+                        })}
+                        <span className={styles.rowIndex} style={{ height: `${renderH}px` }}>
+                          {rowIdx + 1}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-      </div>
+      )}
 
       {pageSize > 1 ? (
         <div className={styles.actionFooter}>
@@ -331,13 +429,13 @@ export default function IndexPage() {
               color="primary"
               size="large"
             >
-              打&nbsp;&nbsp;印
+              打印全部（共{pageSize}页）
             </Button>
           </Form.Item>
         </div>
       ) : null}
 
-      <div className={styles.printAreaA4}>
+      <div className={styles.printAreaA4} style={{fontFamily: fontFamily && fontFamily.key ? fontFamily.key : 'LiSu'}}>
         {Array.from({ length: pageSize }).map((_p, pageIdx) => {
           return (
             <div key={pageIdx} style={{ margin: 0, padding: 0, border: 0, width: `${A4.w}mm`, height: `${A4.h}mm` }}>
