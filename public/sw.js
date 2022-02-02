0db4637ac48e95477f4bccb0ac69eab860de0c96
benlv <992110146@qq.com>
2022-02-02 21:38:18 +0800

    cache update

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Files to cache
-const cacheName = 'js13kPWA-v6';
+const cacheName = 'js13kPWA-v8';
 
 const appShellFiles = [
   '/print/lishu.ttf',
@@ -27,14 +27,20 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     (async () => {
+      const updateLatest = async () => {
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        // console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      };
       const r = await caches.match(e.request);
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) return r;
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
+      if (r) {
+        updateLatest();
+        return r;
+      }
+      return updateLatest();
     })(),
   );
 });
